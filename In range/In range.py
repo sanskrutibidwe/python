bb4cb399c185a4c0ee27e0eb55269dfc8437bce4
sanskrutibidwe <sanskrutibidwe24@gmail.com>
2023-03-05 12:01:54 +0530

    In range

---
+++ In range/In range.py
@@ -0,0 +1,18 @@
+for i in range (1,5):
+    print(i)
+
+
+for i in range (1,5,1):
+    print(i)
+
+
+for i in range (1,10,2):
+    print(i)
+
+
+for i in range (10):
+    print(i)
+
+
+for i in range (10,0,-1):
+    print(i)
