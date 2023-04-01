0bcc9f8a354c94d38bb21e95987433b4526ab878
sanskrutibidwe <sanskrutibidwe24@gmail.com>
2023-04-01 22:30:44 +0530

    area

---
+++ break.py
@@ -0,0 +1,30 @@
+for i in range (5):
+    if i==3:
+        break
+    print(i)
+
+for i in range (10):
+    if i==8:
+        break
+    print(i)
+
+
+for i in range (8):
+    if i==6:
+        break
+    print(i)
+
+
+for i in range (1,5):
+    for j in range (1,5):
+        if j==3:
+            break
+        print(i,j)
+
+
+for i in range (2,10):
+    for j in range (2,10):
+        if j==7:
+            break
+        print(i,j)
+
---
+++ continue.py
@@ -0,0 +1,10 @@
+for i in range (5):
+    if i==3:
+        continue
+    print(i)
+
+
+for i in range (10):
+    if i==7:
+        continue
+    print(i)
